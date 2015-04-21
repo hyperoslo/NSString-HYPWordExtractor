0a1faf544c505614e1100dacdc31ede2378c4eb5
Christoffer Winterkvist <chris@zenangst.com>
2015-04-21 09:55:31 +0200

    Refactor

--- Source/NSString+HYPWordExtractor.m
+++ Source/NSString+HYPWordExtractor.m
@@ -1,45 +1,45 @@
 #import "NSString+HYPWordExtractor.h"
 
+static NSString *const HYPWordCharacterSet = @"ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_[.]0123456789";
+static NSString *const HYPVariableCharacterSet = @"ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_[.]0123456789";
+
 @implementation NSString (HYPWordExtractor)
 
-- (NSArray *)hyp_words
-{
-    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"0123456789ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_"];
+- (NSArray *)hyp_words {
+    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:HYPWordCharacterSet];
 
-    return [self hyp_parseWords:[NSMutableArray new] withCharacterSet:set];
+    return [self hyp_parseWords:[NSMutableArray new]
+               withCharacterSet:set];
 }
 
-- (NSSet *)hyp_uniqueWords
-{
-    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"0123456789ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_"];
+- (NSSet *)hyp_uniqueWords {
+    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:HYPWordCharacterSet];
 
-    return [self hyp_parseWords:[NSMutableSet new] withCharacterSet:set];
+    return [self hyp_parseWords:[NSMutableSet new]
+               withCharacterSet:set];
 }
 
-- (NSArray *)hyp_variables
-{
-    NSCharacterSet *characterSet = [NSCharacterSet characterSetWithCharactersInString:@"ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_[.]0123456789"];
+- (NSArray *)hyp_variables {
+    NSCharacterSet *characterSet = [NSCharacterSet characterSetWithCharactersInString:HYPVariableCharacterSet];
 
     return [self cleanupNumericVariables:[self hyp_parseWords:[NSMutableArray new]
                                              withCharacterSet:characterSet]];
 }
 
-- (NSSet *)hyp_uniqueVariables
-{
-    NSCharacterSet *characterSet = [NSCharacterSet characterSetWithCharactersInString:@"ABCDEFGHIJKOLMNOPQRSTUVWXYZÅÄÆÖØabcdefghijkolmnopqrstuvwxyzåäæöø_[.]0123456789"];
+- (NSSet *)hyp_uniqueVariables {
+    NSCharacterSet *characterSet = [NSCharacterSet characterSetWithCharactersInString:HYPVariableCharacterSet];
 
     NSMutableSet *mutableSet = [NSMutableSet new];
 
     [mutableSet addObjectsFromArray:[self cleanupNumericVariables:[self hyp_parseWords:[NSMutableSet new]
-                                                               withCharacterSet:characterSet]]];
+                                                                      withCharacterSet:characterSet]]];
 
     return [mutableSet copy];
 }
 
 #pragma mark - Private methods
 
-- (id)hyp_parseWords:(id)container withCharacterSet:(NSCharacterSet *)set
-{
+- (id)hyp_parseWords:(id)container withCharacterSet:(NSCharacterSet *)set {
     NSScanner *scanner = [NSScanner scannerWithString:self];
     NSString *word;
 
@@ -48,8 +48,9 @@
             [container addObject:word];
         }
 
-        if (scanner.scanLocation < self.length)
+        if (scanner.scanLocation < self.length) {
             scanner.scanLocation++;
+        }
     }
 
     return [container copy];
