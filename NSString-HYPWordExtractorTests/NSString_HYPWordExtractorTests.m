961aca001801cd5159154fcac0c4fb7f603bcaec
Christoffer Winterkvist <chris@zenangst.com>
2015-04-21 09:52:39 +0200

    Add failing test

--- NSString-HYPWordExtractorTests/NSString_HYPWordExtractorTests.m
+++ NSString-HYPWordExtractorTests/NSString_HYPWordExtractorTests.m
@@ -72,4 +72,11 @@
     XCTAssert(valid, @"Container contain all three words in testString");
 }
 
+- (void)testFlatRelationshipVariables {
+    NSString *testString = @"ticket[0].price * ticket[0].quantity + 10";
+    NSArray *variables = [testString hyp_variables];
+
+    XCTAssertTrue(variables.count == 2);
+}
+
 @end
